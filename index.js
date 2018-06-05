bb005b075ef88c8dbcc1fb5416db8218620077d8
Cyrill <torreskirill@yandex.ru>
2018-06-05 08:01:02 +0300

    add socket.io

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const socket = require('socket.io');
 
 //App setup
 const app = express();
-let srerver = app.listen(4000, () => {
+let server = app.listen(4000, () => {
     console.log('listening to request on port 4000');
 });
 
 //Static file
 app.use(express.static('public'));
+
+//Socket setuo
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('Made socket connection', socket.id);
+});
